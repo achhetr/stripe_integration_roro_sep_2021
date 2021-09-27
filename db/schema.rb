7aaa9be4f4e451fd1013a3a6fbcefa736b7fb6d9
akash chhetri <akash.chhetri1@gmail.com>
2021-09-28 07:54:52 +1000

    Association between jobs and user added
    This will ensure when user will pay for the selected job. It will add the connection with the user
    
    Jobs view UI improved to show case user email
    
    Command used:
    - bundle exec rails g migration addUserReferenceToJob
    - bundle exec rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_27_104350) do
+ActiveRecord::Schema.define(version: 2021_09_27_214629) do
 
   create_table "jobs", force: :cascade do |t|
     t.string "company"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 2021_09_27_104350) do
     t.datetime "paid_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_jobs_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -41,4 +43,5 @@ ActiveRecord::Schema.define(version: 2021_09_27_104350) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "jobs", "users"
 end
