8793723718ecf60d36a400777721d64876422761
akash chhetri <akash.chhetri1@gmail.com>
2021-09-27 20:37:54 +1000

    devise setup for user and information related to credit card added
    User model should have column to save user card details and expiry date of the card and subscription
    
    Gems added:
    - devise
    - bcrypt
    
    Command used
    - bundle exec rails g devise:install
    - bundle exec rails g devise:views
    - bundle exec rails g devise User stripe_id stripe_subscription_id card_brand card_last4 card_exp_month card_exp_year expires_at:datetime
    - bundle exec rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_27_103139) do
+ActiveRecord::Schema.define(version: 2021_09_27_103737) do
 
   create_table "jobs", force: :cascade do |t|
     t.string "company"
@@ -19,6 +19,25 @@ ActiveRecord::Schema.define(version: 2021_09_27_103139) do
     t.datetime "paid_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "stripe_id"
+    t.string "stripe_subscription_id"
+    t.string "card_brand"
+    t.string "card_last4"
+    t.string "card_exp_month"
+    t.string "card_exp_year"
+    t.datetime "expires_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
