cd2c1bbd4a1f1ba856f981ce0eb759f78e6bf3e4
akash chhetri <akash.chhetri1@gmail.com>
2021-09-27 21:07:12 +1000

    Home page added
    Command used:
    - bundle exec rails g controller pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :jobs
+  root to: "pages#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
