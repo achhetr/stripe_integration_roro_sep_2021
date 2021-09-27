5b78f6f6a9db75923eebf9bf4691c3b46c064c12
akash chhetri <akash.chhetri1@gmail.com>
2021-09-27 20:44:35 +1000

    User added with extra column about admin
    Command used:
    - bundle exec rails g migration addAdminToUser
    - bundle exec rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_27_103737) do
+ActiveRecord::Schema.define(version: 2021_09_27_104350) do
 
   create_table "jobs", force: :cascade do |t|
     t.string "company"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 2021_09_27_103737) do
     t.datetime "expires_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
